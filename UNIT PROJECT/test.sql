920f130dfdd191e5b0f479aef1c0447c9fc8fe7f
possibly_nai??? <nfarr26@basecampcodingacademy.org>
2025-09-26 13:51:30 -0500

    Delete UNIT PROJECT directory

--- UNIT PROJECT/test.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Recipes VALUES('Sandwich', 1, 1, 'Lunch', 10, 20, 'bread, and all the other things', 'just do it');
-INSERT INTO Recipes VALUES('Salad', 1, 4, 'Lunch', 10, 20, 'green stuff and other healty things', 'just put it in the bowl');
-
-
-SELECT * FROM Recipes;
